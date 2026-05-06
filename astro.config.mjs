540adc7d7a1eebbd284983b9bf33f4f92c83fd39
last-sociable-orange <semiart@outlook.com>
2026-05-05 22:00:22 -0400

    Added build action file based on Astro's doc

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.circuithub.org',
+	site: 'https://last-sociable-orange.github.io',
 	integrations: [mdx(), sitemap()],
 });
