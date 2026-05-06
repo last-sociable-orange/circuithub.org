cfa5c46069b0b0501ed52abaa4bb86306b6c685f
last-sociable-orange <semiart@outlook.com>
2026-05-05 22:15:32 -0400

    Update astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://last-sociable-orange.github.io',
+	site: 'https://circuithub.org',
 	integrations: [mdx(), sitemap()],
-	base: '/circuithub.org',
 });
